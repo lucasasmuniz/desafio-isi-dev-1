34bf493b192f3e6c81bab555e7c839d60838e401
Lucas Muniz <lucasmunizfk@gmail.com>
2025-06-24 19:05:58 -0300

    feat(layout): create header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,10 @@
+import './styles.css';
+
 export default function Header(){
     return(
         <header>
-            <div>
-                <div>
+            <div className="header-user">
+                <div className="header-user-icon">
                     AM
                 </div>
                 <h2>Arthur Morgan</h2>
