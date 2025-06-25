d0d825ab3903613f63bbf6761d9e132d7f83fd3e
Lucas Muniz <lucasmunizfk@gmail.com>
2025-06-25 11:44:33 -0300

    refactor(theme): Standardize some color variable names and import create file icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './routes'
 import ProductsCatalog from './routes/ProductsCatalog'
+import ProductRegister from './routes/ProductRegister'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate replace to="/products" />} />
         <Route path="products" element={<ProductsCatalog />} />
+        <Route path="products/new" element={<ProductRegister />} />
       </Route>
 
       <Route path="*" element={<Navigate to="/" />} />
